a8b2d02b08d0de75627c2271bebe766cbf9679c3
Redstar <42382875+MyWorkHistory@users.noreply.github.com>
2024-04-03 15:31:43 -0400

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Nav from "./Nav"
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <div className="hero-body">
+        <h1>BLOGBOT BRINGS YOU THE LATEST AND BEST BLOG ARTICLES.</h1> 
+        {/* <p>Learning Is Life</p> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
